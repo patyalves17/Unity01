22ba5653a0e2a682794e1cc862bfa6cfd567a107
logonrm <logonrm@fiap.com.br>
2017-11-27 22:32:05 -0200

    primeira aula

---
+++ Assets/Scripts/Bloco.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bloco : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D c){
+		print ("colidi");
+		//print (c.gameObject.name);
+		//print (c.gameObject.transform.position);
+		//print (c.gameObject.tag);
+		if(c.gameObject.tag == "piso"){
+			Destroy (gameObject); // destoy a si mesmo
+			//Destroy (c.gameObject);//destroy o chao
+		}
+
+	}
+
+	void OnCollisionStay2D(Collision2D c){
+		print ("estou colidindo....");
+	}
+
+
+	void OnCollisionExit2D(Collision2D c){
+		print ("parei de colidir!!!");
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Personagem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personagem : MonoBehaviour {
+
+	public float velocidade;
+
+	// Use this for initialization
+	void Start () {
+		
+		//reposicionar o sprite para coordenada especifica
+		//transform.position = new Vector2 (-10.0f, 0.0f);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.Translate(5.0f*Time.deltaTime, 0.0f, 0.0f);
+		//transform.Translate (Vector2.up*0.5f *Time.deltaTime);
+		//transform.Rotate(Vector3.back * velocidade * Time.deltaTime);
+		//print(Input.GetAxis("Horizontal"));
+
+		/*float move_x = Input.GetAxis("Horizontal") * velocidade * Time.deltaTime;
+		float move_y = Input.GetAxis("Vertical") * velocidade * Time.deltaTime;
+		transform.Translate (move_x, move_y, 0.0f);
+
+		if (Input.GetButtonDown ("Jump")) {
+			transform.position = Vector2.zero;
+		}*/
+
+	}
+}
